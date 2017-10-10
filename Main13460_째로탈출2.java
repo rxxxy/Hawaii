bbb4b28403ef59a0fd8e020bf8bc9694f145eef9
rxxxy <yoozoo.j@gmail.com>
2017-10-11 01:38:57 +0900

    하 고 싶 다 완 성

--- Main13460_째로탈출2.java
+++ Main13460_째로탈출2.java
@@ -12,9 +12,11 @@ public class Main13460_째로탈출2 {
 	static int num2;
 	static int[][] zzero;
 	static int[][] moving = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}};
+	
 	static int[] goal = new int[2];
 	static Deque<int[]> que = new LinkedList<>();
 	static boolean resultFlag;
+	static int resultCount;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -22,8 +24,10 @@ public class Main13460_째로탈출2 {
 		
 		num2 = Integer.parseInt(st.nextToken());
 		num1 = Integer.parseInt(st.nextToken());
-		
 		zzero = new int[num1][num2];
+		int[] redStart = new int[2];
+		int[] blueStart = new int[2];
+		
 		String line = "";
 		for(int i=0; i<num1; i++){
 			line = in.readLine().trim();
@@ -34,10 +38,12 @@ public class Main13460_째로탈출2 {
 					zzero[i][j] = 0;
 				}else if(line.charAt(j)=='R'){
 					zzero[i][j] = -1;
-					que.add(new int[]{i,j,-1,0});
+					redStart[0] = i;
+					redStart[1] = j;
 				}else if(line.charAt(j)=='B'){
 					zzero[i][j] = -2;
-					que.add(new int[]{i,j,-2,0});
+					blueStart[0] = i;
+					blueStart[1] = j;
 				}else if(line.charAt(j)=='O'){
 					zzero[i][j] = 99999;
 					goal[0] = i;
@@ -45,6 +51,14 @@ public class Main13460_째로탈출2 {
 				}
 			}
 		}
+		
+		zzero[redStart[0]][redStart[1]] = 2;
+		zzero[blueStart[0]][blueStart[1]] = 0;
+		que.offer(new int[]{redStart[0], redStart[1], -1, 2});
+		que.offer(new int[]{blueStart[0], blueStart[1], -2, 2});
+		escape();
+		
+		if(resultFlag) System.out.println(resultCount-2);
 		
 		
 	}
@@ -54,152 +68,146 @@ public class Main13460_째로탈출2 {
 		
 		int[] temp1 = null;
 		int[] temp2 = null;
-		int x = 0;
-		int y = 0;
-		int xx = 0;
-		int yy = 0;
+		
+		int redX = 0;
+		int redY = 0;
+		
+		int blueX = 0;
+		int blueY = 0;
+		
 		int count = 0;
 		int whom = 0;
-		boolean flag = false;
-		int flagDir = 0;
+		
+		int tempX = 0;
+		int tempY = 0;
+		
 		
 		while(!que.isEmpty()){
+			tempX = -1;
+			tempY = -1;
 			temp1 = que.poll();
 			temp2 = que.poll();
 			
-		outer:	for(int i=0; i<4; i++){
-				x = temp1[0]+moving[i][0];
-				y = temp1[1]+moving[i][1];
+			
+			//네 방향 모두 돌리기
+outer:			for(int i=0; i<4; i++){
+				if(resultFlag){ return; }
+				
+				//빨간 구슬 먼저 돌리기
 				whom = temp1[2];
 				count = temp1[3];
+				redX = temp1[0]+moving[i][0];
+				redY = temp1[1]+moving[i][1];
 				
-				//빨간 구슬 작업 먼저하기
-				if(x>=0&&x<num1&&y>=0&&y<num2&&(zzero[x][y]!=1||zzero[x][y]>count+1)){
-					while(x>=0&&x<num1&&y>=0&&y<num2&&(zzero[x][y]==0||zzero[x][y]<=count+1)){
-						if(zzero[x][y]>=2 && zzero[x][y]<=count+1){
-							que.offer(new int[]{temp1[0], temp1[1], -1, count});
-							que.offer(new int[]{temp2[0], temp2[1], -2, count});
-							continue outer;
+				//갈 수 있는 방향인지부터 확인하기
+				if(redX>=0&&redX<num1&&redY>=0&&redY<num2&&zzero[redX][redY]!=1){
+					//갈 수 있는 방향인 것을 확인 했으면 빈 칸이 있는 동안 쭉 가기
+					//가려는 길에 블루가 있으면 블루를 먼저 보내고 가고
+					//없으면 레드가 항상 먼저 가는 걸로 하자
+					
+					
+					while(redX>=0&&redX<num1&&redY>=0&&redY<num2){
+						
+						if(zzero[redX][redY]==99999||zzero[redX][redY]==1||(redX==temp2[0]&&redY==temp2[1])){
+							break;
+						}else if(zzero[redX][redY]!=0 && zzero[redX][redY]<=count+1){
+							//이미 갔던 길인데 나보다 더 빨리 간 길이면 갈 필요가 없음
+							continue;
+						}else if(zzero[redX][redY]==0){
+							redX = redX+moving[i][0];
+							redY = redY+moving[i][1];
+							
+						}else if(zzero[redX][redY]>count+1){
+							redX = redX+moving[i][0];
+							redY = redY+moving[i][1];
 						}
-						x += moving[i][0];
-						y += moving[i][1];
 					}
 					
-					if(x>=0&&x<num1&&y>=0&&y<num2){
-						if(zzero[x][y]==1){
-							zzero[x-moving[i][0]][y-moving[i][1]] = count+1;
-							que.offer(new int[]{x-moving[i][0],y-moving[i][1], whom, count+1});
-						}else if(whom==-1 && zzero[x][y]==99999){
-							zzero[goal[0]][goal[1]] = count+1;
-							flag = true;
-						//앞에 파란 구슬이 있는 경우에
-						}else if(zzero[x][y]==-2){
-							//빨간 구슬 다시 넣어주기
-							//파란 구슬 작업 먼저 해주고
-							//그리고 빨간 구슬 다시 돌리기
-							//그리고 continue;
-							xx = temp2[0]+moving[i][0];
-							yy = temp2[1]+moving[i][1];
-								
-							if(zzero[xx][yy]!=1){
-								while(xx>=0&&xx<num1&&yy>=0&&yy<num2&&zzero[xx][yy]==0){
-									xx += moving[i][0];
-									yy += moving[i][1];
-								}
-								
-								if(xx>=0&&xx<num1&&yy>=0&&yy<num2&&zzero[xx][yy]==1){
-									zzero[xx-moving[i][0]*2][yy-moving[i][1]*2] = count+1;
-									que.offer(new int[]{xx-moving[i][0]*2, yy-moving[i][1]*2, -1, count+1});
-									que.offer(new int[]{xx-moving[i][0], yy-moving[i][1], -2, count+1});
-								}else if(xx>=0&&xx<num1&&yy>=0&&yy<num2&&zzero[xx][yy]==99999){
-									System.out.println(-1);
-									return;
-								}
-							}else{
-								que.offer(new int[]{xx-moving[i][0]*2, yy-moving[i][1]*2, -1, count+1});
-								que.offer(new int[]{xx-moving[i][0], yy-moving[i][1], -2, count+1});
-							}
-							continue;
-						}
-					}
+					//나와서 마주친 것이 무엇인지 확인하기
+					//벽의 경우, 출구의 경우, 파란색 구슬의 경우
+					if(zzero[redX][redY]==1){
+						tempX = redX-moving[i][0];
+						tempY = redY-moving[i][1];
+						zzero[tempX][tempY] = count+1;
+						que.offer(new int[]{tempX, tempY, -1, count+1});
+					}else if(zzero[redX][redY]==99999){						
+						resultFlag = true;
+						resultCount = count+1;
+					}else if(redX==temp2[0]&&redY==temp2[1]){
+					//파란색 구슬을 마주친 경우, 파란색 구슬을 먼저 보내고 가야 함
+						whom = temp2[2];
+						blueX = temp2[0]+moving[i][0];
+						blueY = temp2[1]+moving[i][1];
 						
+						if(blueX>=0&&blueX<num1&&blueY>=0&&blueY<num2&&zzero[blueX][blueY]!=1){
+							
+							while(blueX>=0&&blueX<num1&&blueY>=0&&blueY<num2){
+								if(zzero[blueX-moving[i][0]*2][blueY-moving[i][1]*2]<=count+1){
+									continue outer;
+								}else if(zzero[blueX][blueY]!=1){
+									blueX = blueX+moving[i][0];
+									blueY = blueY+moving[i][1];
+									//다른 무언가를 마주침
+								}else if(zzero[blueX][blueY]==1){
+									zzero[blueX-moving[i][0]*2][blueY-moving[i][1]*2] = count+1;
+									que.offer(new int[]{blueX-moving[i][0]*2, blueY-moving[i][1]*2, -1, count+1});
+									que.offer(new int[]{blueX-moving[i][0], blueY-moving[i][1], -2, count+1});
+								}
+							}
+							
+						}
+						continue;
+						
+					}
+				
+				//갈 수 없는 방향이면 continue하기 (blue도 갈 수 없도록)
 				}else{
-					//갈 수 있는 방향이 아닌 방향은 과감하게 continue
-					que.offer(new int[]{temp1[0], temp1[1], -1, count});
-					que.offer(new int[]{temp2[0], temp2[1], -2, count});
 					continue;
 				}
 				
-				xx = temp2[0]+moving[i][0];
-				yy = temp2[1]+moving[i][1];
-				whom = temp2[2];
-				count = temp2[3];
 				
-				//파란 구슬 작업하기
-				if(xx>=0&&xx<num1&&yy>=0&&yy<num2&&zzero[xx][yy]!=1){
-					while(x>=0&&x<num1&&y>=0&&y<num2&&zzero[x][y]==0){
-						x += moving[i][0];
-						y += moving[i][1];
-					}
-					
-					if(x>=0&&x<num1&&y>=0&&y<num2){
-						if(zzero[x][y]==1){
-							zzero[x-moving[i][0]][y-moving[i][1]] = count+1;
-							que.offer(new int[]{x-moving[i][0],y-moving[i][1], whom, count+1});
-						}else if(whom==-1 && zzero[x][y]==99999){
-							zzero[goal[0]][goal[1]] = count+1;
-							flag = true;
-						//앞에 파란 구슬이 있는 경우에
-						}else if(zzero[x][y]==-2){
-							//빨간 구슬 다시 넣어주기
-							//파란 구슬 작업 먼저 해주고
-							//그리고 빨간 구슬 다시 돌리기
-							//그리고 continue;
-							xx = temp2[0]+moving[i][0];
-							yy = temp2[1]+moving[i][1];
-								
-							if(zzero[xx][yy]!=1){
-								while(xx>=0&&xx<num1&&yy>=0&&yy<num2&&zzero[xx][yy]==0){
-									xx += moving[i][0];
-									yy += moving[i][1];
-								}
-								
-								if(xx>=0&&xx<num1&&yy>=0&&yy<num2&&zzero[xx][yy]==1){
-									zzero[xx-moving[i][0]*2][yy-moving[i][1]*2] = count+1;
-									que.offer(new int[]{xx-moving[i][0]*2, yy-moving[i][1]*2, -1, count+1});
-									que.offer(new int[]{xx-moving[i][0], yy-moving[i][1], -2, count+1});
-								}else if(xx>=0&&xx<num1&&yy>=0&&yy<num2&&zzero[xx][yy]==99999){
-									System.out.println(-1);
-									return;
-								}
-							}else{
-								que.offer(new int[]{xx-moving[i][0]*2, yy-moving[i][1]*2, -1, count+1});
-								que.offer(new int[]{xx-moving[i][0], yy-moving[i][1], -2, count+1});
-							}
-							continue;
+				//파란 구슬 돌리기
+				whom = temp2[2];
+				blueX = temp2[0]+moving[i][0];
+				blueY = temp2[1]+moving[i][1];
+				
+				if(blueX>=0&&blueX<num1&&blueY>=0&&blueY<num2&&zzero[blueX][blueY]!=1){
+						
+					while(blueX>=0&&blueX<num1&&blueY>=0&&blueY<num2){
+						if(zzero[blueX][blueY]==99999){
+							System.out.println(-1);
+							resultFlag = false;
+							return;
+						}else if(blueX==tempX&&blueY==tempY){
+							que.offer(new int[]{blueX-moving[i][0], blueY-moving[i][1], -2, count+1});
+							break;
+						}else if(zzero[blueX][blueY]!=1){
+							blueX = blueX+moving[i][0];
+							blueY = blueY+moving[i][1];
+							//다른 무언가를 마주침
+						}else if(zzero[blueX][blueY]==1){
+							que.offer(new int[]{blueX-moving[i][0], blueY-moving[i][1], -2, count+1});
+							break;
 						}
 					}
-						
 				}
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
 			}
 			
-		}// Queue END
+			
+			
+		}
 		
+			
+		
+		for(int i=0; i<num1; i++){
+			for(int j=0; j<num2; j++){
+				System.out.print(zzero[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
 	}
-
-	
 	
 	
 }
